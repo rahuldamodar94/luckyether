6d59b7adc028215ad58329dc20387fcdb02e7fed
Rahul Prabhu <rahulprabhu@Rahuls-MacBook-Pro.local>
2019-03-01 17:35:18 +0530

    added count

--- ether.js
+++ ether.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose');
 
 const options = { useNewUrlParser: true, useCreateIndex: true };
-
+var fs = require('fs');
 mongoose.connect('mongodb://127.0.0.1:27017/account', options);
 var connection = mongoose.connection;
-connection.on("error",console.error.bind(console,"connection error"));
+connection.on("error", console.error.bind(console, "connection error"));
 
 const Web3 = require('web3');
 const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/75aa7935112647bc8cc49d20beafa189');
 const web3 = new Web3(provider);
 var Account = require('./account');
 
-function createAccounts(){
-    var count = 0;
+var count = 0;
+
+function createAccounts() {
+
 
     var accountDetails = web3.eth.accounts.create();
 
@@ -22,8 +24,12 @@ function createAccounts(){
 
     web3.eth.getBalance(account)
         .then((res) => {
+            count++;
+            fs.writeFile('count.txt', count.toString(), function(err) {
+                if (err) throw err;
+            });
             const etherValue = web3.utils.fromWei(res, 'ether');
-            
+
             if (etherValue > 0) {
                 balance = etherValue;
 
@@ -34,7 +40,7 @@ function createAccounts(){
                 });
 
                 details.save().then((doc) => {
-                	count++;
+                    count++;
                     console.log("Added " + count + "records");
                 }).catch((err) => {
                     console.log(err.message);
@@ -46,7 +52,7 @@ function createAccounts(){
         })
 }
 
-var startCreate = setInterval(()=>{
+var startCreate = setInterval(() => {
     clearInterval(startCreate);
     createAccounts();
-},1000);
+}, 1000);
